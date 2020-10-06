44dc9872c36c1eb647822441724e513837a87f79
ctavispost <ctavispost@gmail.com>
2020-10-06 12:07:00 -0700

    move zombies toward player char, increasing position if to the right, and decreasing position if to the left

--- js/app.js
+++ js/app.js
@@ -3,14 +3,15 @@ console.log("Hello?");
 //const playerChar = document.createElement('div');
 //playerChar.setAttribute('class', 'player');
 //document.querySelector('main').appendChild(playerChar);
-let zombieCount = 0;
+
 const monsters = [];
+
+let playerHP = 20;
 
 const createZombie = () => {
     const zombie = document.createElement('div');
     zombie.setAttribute('class', 'zombie');
     document.querySelector('main').appendChild(zombie);
-    zombieCount++;
     const monsterObj = {type: 'zombie', attack: 1, position: 1};
     monsters.push(monsterObj)
 }
@@ -18,15 +19,31 @@ const createZombie = () => {
 const zombTimer = setInterval(createZombie, 2000);
 const zombCreateStop = setTimeout(clearInterval(zombTimer), 20000);
 
+// move zombies toward player char, increasing position if to the right, and decreasing position if to the left
+//MVP will only concern itself with monsters to the right
 const monstersMove = () => {
     const zoms = document.getElementsByClassName(zombie);
-    for (i = 0; i < zoms.length; i++) {
-        monsters[i].postion += 1;
-        const positionName = `zombieLoc ${monsters[i].postion}`
-        const position = zoms[i].setAttribute('class', 'positionName');
+    if (zoms.length > 0){
+        for (i = 0; i < zoms.length; i++) {
+            if (monsters[i].position < 6){
+                monsters[i].postion++;                
+            } else if (monsters[i].position > 7) {
+                monsters[i].position--;
+            }
+            const positionName = `zombieLoc${monsters[i].postion}`
+            const position = zoms[i].setAttribute('class', 'positionName');
+        }
     }
 }
 
-const runGame = () => {
+const getDamage = () => {
+    if ()
+}
 
+const timeMoves = setInterval(monstersMove, 500);
+
+const runGame = () => {
+    zombTimer;
+    zombCreateStop;
+    timeMoves;
 }
