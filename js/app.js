6d3085b63c43993c09c235017e23e63fb568689c
ctavispost <ctavispost@gmail.com>
2020-10-06 11:32:30 -0700

    Create monster array to track pos, type, and attack; set movement logic

--- js/app.js
+++ js/app.js
@@ -4,21 +4,26 @@ console.log("Hello?");
 //playerChar.setAttribute('class', 'player');
 //document.querySelector('main').appendChild(playerChar);
 let zombieCount = 0;
+const monsters = [];
 
 const createZombie = () => {
     const zombie = document.createElement('div');
     zombie.setAttribute('class', 'zombie');
     document.querySelector('main').appendChild(zombie);
     zombieCount++;
+    const monsterObj = {type: 'zombie', attack: 1, position: 1};
+    monsters.push(monsterObj)
 }
 
 const zombTimer = setInterval(createZombie, 2000);
 const zombCreateStop = setTimeout(clearInterval(zombTimer), 20000);
 
-const zombMove = () => {
+const monstersMove = () => {
     const zoms = document.getElementsByClassName(zombie);
-    for (i = 0; i < zombCreateStop.length; i++) {
- //       const position = zoms[i].setAttribute('class', '')
+    for (i = 0; i < zoms.length; i++) {
+        monsters[i].postion += 1;
+        const positionName = `zombieLoc ${monsters[i].postion}`
+        const position = zoms[i].setAttribute('class', 'positionName');
     }
 }
 
