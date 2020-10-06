37c1c4f4fdeabc1454f7e6ae531fa510a028872b
ctavispost <ctavispost@gmail.com>
2020-10-05 17:13:11 -0700

    Begin to layer basic logic for zombies

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-console.log("Hello?")
+console.log("Hello?");
+
+//const playerChar = document.createElement('div');
+//playerChar.setAttribute('class', 'player');
+//document.querySelector('main').appendChild(playerChar);
+let zombieCount = 0;
+
+const createZombie = () => {
+    const zombie = document.createElement('div');
+    zombie.setAttribute('class', 'zombie');
+    document.querySelector('main').appendChild(zombie);
+    zombieCount++;
+}
+
+const zombTimer = setInterval(createZombie, 2000);
+const zombCreateStop = setTimeout(clearInterval(zombTimer), 20000);
+
+const zombMove = () => {
+    const zoms = document.getElementsByClassName(zombie);
+    for (i = 0; i < zombCreateStop.length; i++) {
+ //       const position = zoms[i].setAttribute('class', '')
+    }
+}
+
+const runGame = () => {
+
+}
